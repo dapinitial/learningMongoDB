862970990f703318395641a9ff0c79ca1ed8d749
dapinitial <me@davidpuerto.com>
2016-11-25 00:23:37 -0800

    Test for finding a particular record.

--- users/test/read_test.js
+++ users/test/read_test.js
@@ -10,6 +10,8 @@ describe('Reading users out of the database', () => {
 			.then(() => done());
 	});
 
+	// FIND ALL USERS NAMED: DAVID
+
 	it('Finds all the users named David', (done) => {
 		User.find({ name: 'David' })
 		.then((users) => {
@@ -20,6 +22,7 @@ describe('Reading users out of the database', () => {
 				* is different and fails a triple equals comparisson!
 				* MongoDB stores ids as ObjectId("5837c269c1d1c78c39b44cd5")
 				* but the ._id returns: 5837c269c1d1c78c39b44cd5
+				* try the two console logs below.
 				*/
 
 			// console.log(users[0]._id);
@@ -28,6 +31,16 @@ describe('Reading users out of the database', () => {
 			assert(users[0]._id.toString() === david._id.toString())
 			done();
 		});		
+	});
+
+	// FIND A PARTICULAR RECORD
+
+	it('Find a user with a particular ID', (done) => {
+		User.findOne({ _id: david._id })
+			.then((user) => {
+				assert(user.name === 'David');
+				done();
+			});
 	});
 
 });
