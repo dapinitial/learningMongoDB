d32763a819f7e9d5ca8a8a5fc24b6b1ba5b0d9bc
David Puerto <me@davidpuerto.com>
2016-12-01 22:54:49 -0800

    fixing a bug with posts not populating

--- users/test/association_test.js
+++ users/test/association_test.js
@@ -5,51 +5,49 @@ const Comment = require('../src/comment');
 const BlogPost = require('../src/blogPost');
 
 describe('Assocations', () => {
-	let david, blogPost, comment;
+  let david, blogPost, comment;
 
-	beforeEach((done) => {
-		david = new User({ name: 'David' });
-		blogPost = new BlogPost({ title: 'JS is Great', content: 'Yep it really is' });
-		comment = new Comment({ content: 'Congrats on great post' });
+  beforeEach((done) => {
+    david = new User({ name: 'David' });
+    blogPost = new BlogPost({ title: 'JS is Great', content: 'Yep it really is' });
+    comment = new Comment({ content: 'Congrats on great post' });
 
-		david.blogPosts.push(blogPost);
-		blogPost.comments.push(comment);
-		comment.user = david;
+    david.blogPosts.push(blogPost);
+    blogPost.comments.push(comment);
+    comment.user = david;
 
-		Promise.all([david.save(), blogPost.save(), comment.save()])
-		.then(() => done());
-	});
+    Promise.all([david.save(), blogPost.save(), comment.save()])
+      .then(() => done());
+  });
 
-	it('saves a relation between a user and a blogpost', (done) => {
-		User.findOne({ name: 'David' })
-		.populate('blogPosts')
-		.then((user) => {
-			console.log(user.blogPosts)
-			assert(user.blogPosts[0].title === 'JS is Great');
-			done();
-		});
-	});
+  it('saves a relation between a user and a blogpost', (done) => {
+    User.findOne({ name: 'David' })
+      .populate('blogPosts')
+      .then((user) => {
+        assert(user.blogPosts[0].title === 'JS is Great');
+        done();
+      });
+  });
 
-	it('saves a full relation graph', (done) => {
-		User.findOne({ name: 'David' })
-		.populate({
-			path: 'blogPosts',
-			populate: {
-				path: 'comments',
-				model: 'comment',
-				populate: {
-					path: 'user',
-					model: 'user'
-				}
-			}
-		})
-		.then((user) => {
-			console.log(user.blogPosts[0].comments);
-			assert(user.name === 'David');
-			assert(user.blogPosts[0].title === 'JS is Great');
-			assert(user.blogPosts[0].comments[0].content === 'Congrats on great post');
-			assert(user.blogPosts[0].comments[0].user.name === 'David');
-			done();
-		});
-	});
+  it('saves a full relation graph', (done) => {
+    User.findOne({ name: 'David' })
+      .populate({
+        path: 'blogPosts',
+        populate: {
+          path: 'comments',
+          model: 'comment',
+          populate: {
+            path: 'user',
+            model: 'user'          }
+        }
+      })
+      .then((user) => {
+        //console.log(user.blogPosts[0].comments[0]);
+        assert(user.name === 'David');
+        assert(user.blogPosts[0].title === 'JS is Great');
+        assert(user.blogPosts[0].comments[0].content === 'Congrats on great post');
+        assert(user.blogPosts[0].comments[0].user.name === 'David');
+        done();
+      });
+  });
 });
